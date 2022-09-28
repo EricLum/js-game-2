046bb247f2c3b74c4569784221e880da3dbb6543
eal6gf <elum1234@gmail.com>
2022-09-28 16:55:23 -0400

    refactored player logic

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ const initBoard = () => {
 };
 
 const initPlayer = () => {
-  const player = document.getElementById("player");
-  withMovement(player);
-  // paste("board", player)
+    const player = new Player()
 };
 
 const paste = (parentElementId = "main", element) => {
@@ -25,54 +23,73 @@ const paste = (parentElementId = "main", element) => {
   parent.appendChild(element);
 };
 
-const withMovement = (element) => {
-  window.addEventListener("keydown", (e) => onKeyPress(e, element));
-  return element;
-};
-
-const onKeyPress = (e, element) => {
-  const code = e.keyCode;
-
-  const LEFT = "left";
-  const RIGHT = "right";
-  const UP = "up";
-  const DOWN = "down";
-  const MAP_ARROW_KEY_CODE_TO_DIR = {
-    37: LEFT,
-    38: UP,
-    39: RIGHT,
-    83: RIGHT,
-    40: DOWN,
-  };
-  const direction = Object.keys(MAP_ARROW_KEY_CODE_TO_DIR).includes(
-    code.toString()
-  )
-    ? MAP_ARROW_KEY_CODE_TO_DIR[code]
-    : null;
-
-  if (direction) {
-    const p = getElement("player");
-    switch (direction) {
-      case RIGHT:
-        p.style.left = getPixels(p.style.left) + 50 + "px";
-        break;
-      case LEFT:
-        p.style.left = getPixels(p.style.left) + -50 + "px";
-        break;
-      case UP:
-        p.style.top = getPixels(p.style.top) + -50 + "px";
-        break;
-      case DOWN:
-        p.style.top = getPixels(p.style.top) + 50 + "px";
-        break;
-    }
-  }
-};
 
 const getPixels = (s) => {
   const res = parseInt(s.split("px")[0]);
-  console.log(res);
   return res || 0;
 };
 
 const getElement = (id) => document.getElementById(id);
+
+class Player {
+  constructor() {
+    this.id = "player";
+    this.x = getPixels(getElement(this.id).style.left);
+    this.y = getPixels(getElement(this.id).style.top);
+    this.baseMovementUnit = 30;
+    window.addEventListener("keydown", (e) => onKeyPress(e, getElement(this.id)));
+  }
+
+  handleMovement = () => {
+    const code = e.keyCode;
+    const LEFT = "left";
+    const RIGHT = "right";
+    const UP = "up";
+    const DOWN = "down";
+    const MAP_ARROW_KEY_CODE_TO_DIR = {
+      37: LEFT,
+      38: UP,
+      39: RIGHT,
+      83: RIGHT,
+      40: DOWN,
+    };
+    const direction = Object.keys(MAP_ARROW_KEY_CODE_TO_DIR).includes(
+      code.toString()
+    )
+      ? MAP_ARROW_KEY_CODE_TO_DIR[code]
+      : null;
+
+    if (direction) {
+      switch (direction) {
+        case RIGHT:
+          this.moveX(this.baseMovementUnit);
+          break;
+        case LEFT:
+          this.moveX(-1 * this.baseMovementUnit);
+          break;
+        case UP:
+          this.moveY(-1 * this.baseMovementUnit);
+          break;
+        case DOWN:
+          this.moveY(this.baseMovementUnit);
+          break;
+      }
+    }
+  };
+
+  moveX = (dir) => {
+    this.x = getPixels(p.style.left) + dir + "px";
+    this.setPosition();
+  };
+
+  moveY = (dir) => {
+    this.y = getPixels(p.style.top) + dir + "px";
+    this.setPosition();
+  };
+
+  setPosition = () => {
+    const element = getElement(this.id);
+    element.style.left = this.x;
+    element.style.top = this.y;
+  };
+}
